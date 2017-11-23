fbb50a73d1e3a91700b6c311ccedffe51fba2fd4
Chocla <chocla1@cock.li>
2017-11-23 13:59:22 -0800

    Problem 62 - New Approach that actually works

--- problem62/main.go
+++ problem62/main.go
@@ -2,105 +2,94 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"strconv"
+	"sort"
 )
 //Find the smallest cube for which exactly five permutations of its digits are cube.
 
-
+type cube struct {
+	o []int // ordered digits
+	t int   // times appeared
+	l int   // lowest root
+}
 func main() {
-	//fmt.Println(findAllPermutations([]int{1,0,3,0,3,0,1}))
-	
-	 i := 0
-	answerFound := false;
-	for ;!answerFound; i++ {
-		answerFound = checkPermutationsforCubes(int(math.Pow(float64(i),3)))
-		fmt.Println(i)
+	answer := 0
+	i := 0
+	var cubeList []cube
+	initCube := cube{[]int{0},1,0}
+	cubeList = append(cubeList, initCube)
+	answerFound := false
+
+	for !answerFound {
+		i++
+		tmpCube := orderDigits(i*i*i)
+		inList := false
+
+		for j := 0; j < len(cubeList); j++ {
+
+			for k := 0; k < len(tmpCube); k++ {
+				if testEq(cubeList[j].o, tmpCube) {
+					inList = true
+				}
+			}
+			if inList {
+				cubeList[j].t++
+				break
+			}
+
+
+		}
+		if !inList {
+			newCube := cube{tmpCube,1,i}
+			cubeList = append(cubeList, newCube)
+		}
+
+		for j := 0; j < len(cubeList);j++ {
+			if cubeList[j].t ==5 {
+				fmt.Println(cubeList[j])
+				answer = cubeList[j].l
+				answerFound = true
+			}
+		}
+
 	}
-	fmt.Println("i:", i)
-	fmt.Println("Answer:", i)
+	fmt.Println("Answer:", answer*answer*answer)
 }
 
-func checkPermutationsforCubes(inCube int)(x bool){
-	//fmt.Println(inCube)
-	digits := getDigits(inCube)
-	//fmt.Println(digits)
-	numCubes := findAllPermutations(digits)
-	//testing case
-	//fmt.Println(numCubes)
-	if numCubes == 5 {
+func testEq(a, b []int) bool {
+	
+		if a == nil && b == nil { 
+			return true; 
+		}
+	
+		if a == nil || b == nil { 
+			return false; 
+		}
+	
+		if len(a) != len(b) {
+			return false
+		}
+	
+		for i := range a {
+			if a[i] != b[i] {
+				return false
+			}
+		}
+	
 		return true
 	}
-	return false
-}
+func orderDigits (i int) (o []int) {
+	var digitSlice sort.IntSlice
+	x := strconv.Itoa(i)
 
-//Implementation of heap's algorithm for permutations
-func findAllPermutations(input []int) (numCubes int){
-	c := make([]int, len(input))
-	cubes := make([]int, 0)
-
-	for i := 0; i < len(input); {
-		if c[i] < i {
-			if i % 2 == 0 {
-				swap(&input[0], &input[i])
-			} else {
-				swap(&input[c[i]], &input[i])
-			}
-			num,isCube := checkCube(input)
-			if isCube{
-				uniqueCube := true
-				for i := 0; i < len(cubes);i++{
-					if num == cubes[i] {
-						uniqueCube = false
-					}
-				}
-				if uniqueCube {
-					cubes = append(cubes, num)
-				}
-			}
-			c[i]++
-			i = 0
-		} else {
-			c[i] = 0
-			i += 1
-		}
+	for j := 0; j < len(x); j++ {
+		digitSlice = append(digitSlice, int(x[j] - '0'))
 	}
-	//fmt.Println(cubes)
-	return len(cubes)
-}
-func swap(a, b *int) {
-	var tmp int = *a
-	*a = *b
-	*b = tmp
 
-}
-func getDigits(input int)(digitSlice []int) {
-	x := strconv.Itoa(input)
-	//fmt.Println(x)
-	for i := 0; i < len(x); i++ {
-		digitSlice = append(digitSlice, int(x[i] - '0'))
-	}
+	digitSlice.Sort()
+	o = digitSlice
 
 	return
 }
 
-func checkCube(input []int) (num int, x bool) {
-	//convert back into one number
 
-	place := 1
-	for i := len(input) - 1; i >= 0; i-- {
-		num += input[i]*place
-		place *= 10
-	}
-	if num < place / 10 {
-		return 0,false
-	}
-	cbrt := math.Cbrt(float64(num))
-	//fmt.Println(cbrt,math.Floor(cbrt))
-	if cbrt - math.Floor(cbrt) < 0.0000000000000000000000001 {
-		return num,true
-	}
-	return 0,false
-	//check the cuberoot
-		//if it's a perfect cube, return true
-}
