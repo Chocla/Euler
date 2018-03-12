22428648a75d956f626003f2c2cfaf16630e990d
Chocla <19178866+Chocla@users.noreply.github.com>
2018-03-12 12:09:58 -0700

    Reformat repository structure

--- problem073/main.go
+++ problem073/main.go
@@ -23,6 +23,9 @@ func findIndex(N, num, den int) (int) {
 	i := 0
 	a, b, c, d := 0,1,1,N
 	for (a != 1 || b != 1){
+		//the next term is defined in terms of the previous two terms:
+		//p = floor((N + b) / d) * c - a
+		//q = floor((N + b) / d) * d - b
 		k := (N + b) / d
 		a, b, c, d = c, d, (k * c - a), (k * d - b)
 		if a == num && b == den {
