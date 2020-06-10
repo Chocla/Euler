fd1d2c9b3604ee82d30bd74af8e869e9ef892139
Chocla <joe.mcdonough@live.com>
2020-06-10 15:56:44 -0700

    Problem 104 - Pandigital Fibonacci ends

---
+++ problem104/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"sort"
+	"time"
+)
+
+type sortRunes []rune 
+func (s sortRunes) Less(i, j int) bool { return s[i] < s[j] }
+func (s sortRunes) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s sortRunes) Len() int { return len(s) }
+
+func SortString(s string) string {
+    r := []rune(s)
+    sort.Sort(sortRunes(r))
+    return string(r)
+}
+
+func main() {
+	t0 := time.Now()
+	ans := findPandigitalFib()
+	fmt.Println("Answer:",ans,"\nTime:",time.Since(t0))
+	
+}
+
+func findPandigitalFib() (int){
+
+	a := big.NewInt(1)
+	b := big.NewInt(1)
+
+	for i := 3; ; i++ {
+		a.Add(a,b)
+		a, b = b, a
+		// fmt.Println(i, b) //b is F_i in terms of this problem
+		str := b.String()
+		if i > 45{
+			last := SortString(str[len(str) - 9:])
+			first := SortString(str[:9])
+			if last == "123456789" && first == "123456789" {
+				return i
+			}
+		}	
+	}
+	return -1
+}
